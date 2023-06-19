fa06196fdbf9f21ebffbf02245a6d159f5f6e2f2
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-06-19 20:18:41 +0530

    added 2,3

---
+++ 2_echoAndPrint.php
@@ -0,0 +1,11 @@
+<?php
+    //we can print something in php using echo,print statements and various functions like print(),print_r(),etc.
+
+    //just remember that echo is a statement/command and not a function
+    echo "Hello World!";
+    $name="User";
+    echo "<br>Hello" . $name . "<br>";  //  .   concatenates/adds strings.  //<br> is a html tag which can also be used in php, it should be within " "
+
+    //using print function
+    print("Hello". $name . "<br>");
+?>
---
+++ 3_specialOperators.php
@@ -0,0 +1,7 @@
+<?php
+    $var1=2;
+    $var2=3;
+    
+    $var3=$var1**$var2;         // ** indicates var1 to the power var2 i.e -> var1 multiplied var2 times
+
+?>

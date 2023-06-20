302b373505466743437153c07a269d0bfb9acb0b
Chirag Suryawanshi <chiragsuryavanshi01@gmail.com>
2023-06-20 15:00:55 +0530

    7,8,9

---
+++ 8_variableFunction.php
@@ -0,0 +1,9 @@
+<?php
+    //in var function we assign a variable the name of function (string) and then call the variable to call the original function 
+    function wow(){
+        echo "wow";
+    }
+    $func="wow";
+    $func();
+
+?>
---
+++ 9_anonymousFunctions.php
@@ -0,0 +1,8 @@
+<?php
+    //Anonymous function is a function without any user defined name
+    
+    $var=function(){
+        echo "Hello";
+    };
+    $var();
+?>
